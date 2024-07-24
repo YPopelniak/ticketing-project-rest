1136bf9b4e5ac725dfe4ce25fbb791d352ddf1d4
Yuliia Popelniak <popelniak96@gmail.com>
2024-07-23 22:54:16 -0500

    Initial Commit

---
+++ src/main/java/com/cydeo/dto/TaskDTO.java
@@ -0,0 +1,22 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.Status;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TaskDTO {
+
+    private Long id;
+    private ProjectDTO project;
+    private UserDTO assignedEmployee;
+    private String taskSubject;
+    private String taskDetail;
+    private Status taskStatus;
+    private LocalDate assignedDate;
+}
